479e92b55aa4986c15bc1520172a70240bc7cf5b
Cameron Malone <cameron@launchkeylab.com>
2025-10-01 10:26:52 +1000

    style: formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,113 +1,105 @@
 return {
-  -- -------------------------------------------------
-  --               MASON                             |
-  -- -------------------------------------------------
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- go
-        "gopls",
-        "gofumpt",
-        "goimports",
-        -- c
-        "clangd",
-        "codelldb",
-        -- js-ts-web
-        "vtsls",
-        "prettier",
-        "prettierd",
-        "tailwindcss-language-server",
-        "css-lsp",
-        "emmet-language-server",
-        "json-lsp",
-        "svelte-language-server",
-        -- python
-        "ruff",
-        "pyright",
-        -- sh
-        "bash-language-server",
-        -- zig
-        "zls",
-        -- lua
-        "lua-language-server",
-      },
-    },
-  },
-  -- -------------------------------------------------
-  --               LSPCONFIG                         |
-  -- -------------------------------------------------
-
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      inlay_hints = { enabled = false },
-      servers = {
-        pyright = {},
-        emmet_language_server = {},
-        bashls = {},
-        cssls = {},
-        tailwindcss = {},
-        astro = {},
-        zls = {},
-        gleam = {},
-        vtsls = {},
-        ruff_lsp = {},
-        gopls = {
-          settings = { gopls = { analyses = { fieldalignment = false } } },
+    -- -------------------------------------------------
+    --               MASON                             |
+    -- -------------------------------------------------
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {
+                -- go
+                "gopls",
+                "gofumpt",
+                "goimports",
+                -- c
+                "clangd",
+                "codelldb",
+                -- js-ts-web
+                "vtsls",
+                "prettier",
+                "prettierd",
+                "tailwindcss-language-server",
+                "css-lsp",
+                "emmet-language-server",
+                "json-lsp",
+                "svelte-language-server",
+                -- python
+                "ruff",
+                "pyright",
+                -- sh
+                "bash-language-server",
+                -- zig
+                "zls",
+                -- lua
+                "lua-language-server",
+            },
         },
-      },
-      setup = {
-        rust_analyzer = function()
-          return true
-        end,
-      },
     },
-  },
-  -- -------------------------------------------------
-  --               DIAGFLOW                          |
-  --                                                 |
-  --               Better placement for              |
-  --               LSP Diagnostic Text               |
-  -- -------------------------------------------------
+    -- -------------------------------------------------
+    --               LSPCONFIG                         |
+    -- -------------------------------------------------
 
-  {
-    "dgagn/diagflow.nvim",
-    event = "LspAttach",
-    opts = {
-      format = function(diagnostic)
-        return "[LSP] " .. diagnostic.message
-      end,
+    {
+        "neovim/nvim-lspconfig",
+        ---@class PluginLspOpts
+        opts = {
+            inlay_hints = { enabled = false },
+            servers = {
+                pyright = {},
+                emmet_language_server = {},
+                bashls = {},
+                cssls = {},
+                tailwindcss = {},
+                astro = {},
+                zls = {},
+                gleam = {},
+                vtsls = {},
+                ruff_lsp = {},
+                gopls = {
+                    settings = { gopls = { analyses = { fieldalignment = false } } },
+                },
+                lua_ls = {},
+            },
+            setup = {
+                rust_analyzer = function()
+                    return true
+                end,
+            },
+        },
     },
-  },
+    -- -------------------------------------------------
+    --               DIAGFLOW                          |
+    --                                                 |
+    --               Better placement for              |
+    --               LSP Diagnostic Text               |
+    -- -------------------------------------------------
 
-  -- -------------------------------------------------
-  --               BLINK.CMP                         |
-  --                                                 |
-  --               Lightning Fast Cmp                |
-  --                                                 |
-  -- -------------------------------------------------
-  {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
+    {
+        "dgagn/diagflow.nvim",
+        event = "LspAttach",
+        opts = {
+            format = function(diagnostic)
+                local clients = vim.lsp.buf_get_clients()
+                for _, client in ipairs(clients) do
+                    return client.name .. ": " .. diagnostic.message
+                end
+            end,
         },
-      },
-      completion = {
-        documentation = {
-          auto_show = false,
-        },
-      },
     },
-  },
+
+    -- -------------------------------------------------
+    --               BLINK.CMP                         |
+    --                                                 |
+    --               Lightning Fast Cmp                |
+    --                                                 |
+    -- -------------------------------------------------
+    {
+        "saghen/blink.cmp",
+        opts = {
+            completion = {
+                documentation = {
+                    auto_show = false,
+                },
+            },
+        },
+    },
 }
