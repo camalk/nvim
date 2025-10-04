da00b4de47d852dc5a4ea89f04cb5f298e878508
Cameron Malone <cameron@launchkeylab.com>
2025-10-04 13:22:50 +1000

    feat(snacks): add random startup art

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,39 +1,22 @@
-local cat = [[
-                                
-                                
-                   __________   
-       ^----^     | bahrain |   
-      |(|)(|)|   /-----------   
-     / ~~v~~  \ /               
-     | _____  |                 
-     \  ( (   /                 
-       ------                   
-         ||    __               
-        /  \  /  \              
-        \||/_/                  
-                                
-                                
-]]
-
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    scroll = {
-      -- your scroll configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      enabled = false,
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+        scroll = {
+            -- your scroll configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            enabled = false,
+        },
+        -- this one highlights lsp occurrences - can cause visual clutter
+        words = { enabled = false },
+        indent = {
+            enabled = false,
+        },
+        dashboard = {
+            preset = {
+                header = require("config.dashboards").get_random(),
+            },
+        },
     },
-    -- this one highlights lsp occurrences - can cause visual clutter
-    words = { enabled = false },
-    indent = {
-      enabled = false,
-    },
-    dashboard = {
-      preset = {
-        header = cat,
-      },
-    },
-  },
 }
