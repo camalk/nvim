828b6a0efd4d8bb5796ebb7a36e798d16a9ad9e4
Cameron Malone <cameron@launchkeylab.com>
2025-10-23 09:19:57 +1100

    refactor: move autopairs to utils

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,51 +1,59 @@
 return {
-  -- -------------------------------------------------
-  --               VIM-TMUX-NAVIGATOR                |
-  -- -------------------------------------------------
-  {
-    "alexghergh/nvim-tmux-navigation",
-    opts = {
-      disable_when_zoomed = true,
+    -- -------------------------------------------------
+    --               VIM-TMUX-NAVIGATOR                |
+    -- -------------------------------------------------
+    {
+        "alexghergh/nvim-tmux-navigation",
+        opts = {
+            disable_when_zoomed = true,
+        },
+        keys = {
+            { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", desc = "Navigate Left (tmux aware)" },
+            { "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", desc = "Navigate Right (tmux aware)" },
+            { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", desc = "Navigate Down (tmux aware)" },
+            { "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", desc = "Navigate Up (tmux aware)" },
+        },
     },
-    keys = {
-      { "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>", desc = "Navigate Left (tmux aware)" },
-      { "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>", desc = "Navigate Right (tmux aware)" },
-      { "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>", desc = "Navigate Down (tmux aware)" },
-      { "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>", desc = "Navigate Up (tmux aware)" },
+    -- -------------------------------------------------
+    --               TABOUT                            |
+    -- -------------------------------------------------
+    {
+        "abecodes/tabout.nvim",
+        lazy = true,
+        event = "InsertEnter",
+        opts = {
+            tabouts = {
+                { open = "'", close = "'" },
+                { open = '"', close = '"' },
+                { open = "`", close = "`" },
+                { open = "(", close = ")" },
+                { open = "[", close = "]" },
+                { open = "{", close = "}" },
+                { open = "<", close = ">" },
+            },
+            exclude = {}, -- tabout will ignore these filetypes
+        },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
     },
-  },
-  -- -------------------------------------------------
-  --               TABOUT                            |
-  -- -------------------------------------------------
-  {
-    "abecodes/tabout.nvim",
-    lazy = true,
-    event = "InsertEnter",
-    opts = {
-      tabouts = {
-        { open = "'", close = "'" },
-        { open = '"', close = '"' },
-        { open = "`", close = "`" },
-        { open = "(", close = ")" },
-        { open = "[", close = "]" },
-        { open = "{", close = "}" },
-        { open = "<", close = ">" },
-      },
-      exclude = {}, -- tabout will ignore these filetypes
+    -- -------------------------------------------------
+    --             NVIM SURROUND                       |
+    -- -------------------------------------------------
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
     },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
+    -- -------------------------------------------------
+    --             NVIM AUTOPAIRS                      |
+    -- -------------------------------------------------
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
     },
-  },
-  -- -------------------------------------------------
-  --             NVIM SURROUND                       |
-  -- -------------------------------------------------
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
 }
