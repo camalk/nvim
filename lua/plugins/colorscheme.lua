9810f2421ebc4cb9dd230ee1009b9b7bd3ce21f8
Cameron Malone <cameron@launchkeylab.com>
2025-10-23 09:21:22 +1100

    feat(colorscheme): add conifer and remove unused

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,37 +1,19 @@
 return {
-  -- -------------------------------------------------
-  --               COLORSCHEME                       |
-  -- -------------------------------------------------
-  -- {
-  --   "sainnhe/gruvbox-material",
-  --   init = function()
-  --     vim.g.gruvbox_material_background = "hard"
-  --     vim.g.gruvbox_material_better_performance = 1
-  --   end,
-  -- },
-  {
-    "vague2k/vague.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme vague")
-    end,
-  },
-  -- {
-  --   "wnkz/monoglow.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function(_, opts)
-  --     require("monoglow").setup(opts)
-  --
-  --     vim.api.nvim_set_hl(0, "@string.documentation.python", { link = "Comment" })
-  --   end,
-  -- },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "vague",
+    -- -------------------------------------------------
+    --               COLORSCHEME                       |
+    -- -------------------------------------------------
+    {
+        "lucasadelino/conifer.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            variant = "lunar",
+        },
     },
-  },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "conifer-lunar",
+        },
+    },
 }
