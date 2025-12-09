4fef011226201255a199e8ee1128f71d870107b9
Cameron Malone <cameron@launchkeylab.com>
2025-12-09 12:31:56 +1100

    chore(ts): add all installed parsers to ensure_installed list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,57 @@
 return {
-  -- -------------------------------------------------
-  --               TREESITTER                        |
-  -- -------------------------------------------------
-  -- Treesitter auto install parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "c_sharp" })
-        vim.list_extend(opts.ensure_installed, { "rust" })
-        vim.list_extend(opts.ensure_installed, { "typescript" })
-        vim.list_extend(opts.ensure_installed, { "astro" })
-        vim.list_extend(opts.ensure_installed, { "ocaml" })
-        vim.list_extend(opts.ensure_installed, { "nix" })
-      end
-    end,
-  },
+    -- -------------------------------------------------
+    --               TREESITTER                        |
+    -- -------------------------------------------------
+    -- Treesitter auto install parsers
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            if type(opts.ensure_installed) == "table" then
+                vim.list_extend(opts.ensure_installed, {
+                    "astro",
+                    "bash",
+                    "c",
+                    "c_sharp",
+                    "cpp",
+                    "diff",
+                    "dockerfile",
+                    "dtd",
+                    "gleam",
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "gowork",
+                    "hcl",
+                    "html",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "json5",
+                    "jsonc",
+                    "lua",
+                    "luadoc",
+                    "luap",
+                    "markdown",
+                    "markdown_inline",
+                    "nix",
+                    "ocaml",
+                    "printf",
+                    "python",
+                    "query",
+                    "regex",
+                    "ron",
+                    "rust",
+                    "svelte",
+                    "terraform",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "xml",
+                    "yaml",
+                })
+            end
+        end,
+    },
 }
